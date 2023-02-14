68a063bf510abb9abd6e2d8dba782980eab12cc8
Marpace <almonacid.pablo@hotmail.com>
2023-02-14 14:58:37 -0500

    update code

--- src/components/FormSteps.js
+++ src/components/FormSteps.js
@@ -1,56 +1,35 @@
 function FormSteps(props) {
 
-  function handleStepClick(e) {
-    props.setCurrentStep(Number(e.target.id))
-  }
-
   return (
     <div className="form-steps">
-      <div onClick={handleStepClick} id="1" className="form-steps__item">
-        <span id="1" className={`step-number ${props.currentStep === 1 ? "step-active" : ""}`}>1</span>
+      <div onClick={() => props.setCurrentStep(1)} className="form-steps__item">
+        <span className={`step-number ${props.currentStep === 1 ? "step-active" : ""}`}>1</span>
         <div>
-          <p id="1" className="step-line-one">STEP 1</p>
-          <p id="1" className="step-line-two">YOUR INFO</p>
+          <p className="step-line-one">STEP 1</p>
+          <p className="step-line-two">YOUR INFO</p>
         </div>
       </div>
-      <div onClick={handleStepClick} id="2" className="form-steps__item">
-        <span id="2" className={`step-number ${props.currentStep === 2 ? "step-active" : ""}`}>2</span>
+      <div onClick={() => props.setCurrentStep(2)} className="form-steps__item">
+        <span className={`step-number ${props.currentStep === 2 ? "step-active" : ""}`}>2</span>
         <div>
-          <p id="2" className="step-line-one">STEP 2</p>
-          <p id="2" className="step-line-two">SELECT PLAN</p>
+          <p className="step-line-one">STEP 2</p>
+          <p className="step-line-two">SELECT PLAN</p>
         </div>
       </div>
-      <div onClick={handleStepClick} id="3" className="form-steps__item">
-        <span id="3" className={`step-number ${props.currentStep === 3 ? "step-active" : ""}`}>3</span>
+      <div onClick={() => props.setCurrentStep(3)} className="form-steps__item">
+        <span className={`step-number ${props.currentStep === 3 ? "step-active" : ""}`}>3</span>
         <div>
-          <p id="3" className="step-line-one">STEP 3</p>
-          <p id="3" className="step-line-two">ADD-ONS</p>
+          <p className="step-line-one">STEP 3</p>
+          <p className="step-line-two">ADD-ONS</p>
         </div>
       </div>
-      <div onClick={handleStepClick} id="4" className="form-steps__item">
-        <span id="4" className={`step-number ${props.currentStep === 4 ? "step-active" : ""}`}>4</span>
+      <div onClick={() => props.setCurrentStep(4)} className="form-steps__item">
+        <span className={`step-number ${props.currentStep === 4 ? "step-active" : ""}`}>4</span>
         <div>
-          <p id="4" className="step-line-one">STEP 4</p>
-          <p id="4" className="step-line-two">SUMMARY</p>
+          <p className="step-line-one">STEP 4</p>
+          <p className="step-line-two">SUMMARY</p>
         </div>
       </div>
-      
-      {/* <span 
-        id="1"
-        onClick={handleStepClick} 
-        className={`form-steps__item ${props.currentStep === 1 ? "step-active" : ""}`}>1</span>
-      <span 
-        id="2"
-        onClick={handleStepClick} 
-        className={`form-steps__item ${props.currentStep === 2 ? "step-active" : ""}`}>2</span>
-      <span 
-        id="3"
-        onClick={handleStepClick} 
-        className={`form-steps__item ${props.currentStep === 3 ? "step-active" : ""}`}>3</span>
-      <span 
-        id="4"
-        onClick={handleStepClick} 
-        className={`form-steps__item ${props.currentStep === 4 ? "step-active" : ""}`}>4</span> */}
     </div>
   )
 }
